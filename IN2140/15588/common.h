f1af1c187490627d7b9b1b7ec00bf8d987707974
Saab Singh <saab6a@gmail.com>
2022-10-11 15:04:11 +0200

    IN2000
    Innleveringer, oppgaver og et større prosjekt i samarbeid meteorologisk institutt gjort i faget "Software Engineering med prosjektarbeid" - IN2000, vår 2022

---
+++ IN2140/15588/common.h
@@ -0,0 +1,15 @@
+#ifndef MY_COMMONS
+#define MY_COMMONS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <arpa/inet.h> 
+
+#endif
